21acff4f04611b615d05328a0a81f18ff695fc3a
jeffreyyong <jeffreyyong10@gmail.com>
2021-06-21 00:16:29 +0100

    split out domain files

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -13,49 +13,6 @@ var (
 	ErrUnprocessable            = errors.New("unprocessable")
 )
 
-type PaymentActionType string
-type PaymentActionStatus string
-type TransactionStatus string
-
-func (p PaymentActionType) String() string {
-	return string(p)
-}
-
-const (
-	PaymentActionTypeAuthorization PaymentActionType = "authorization"
-	PaymentActionTypeVoid          PaymentActionType = "void"
-	PaymentActionTypeCapture       PaymentActionType = "capture"
-	PaymentActionTypeRefund        PaymentActionType = "refund"
-)
-
-const (
-	PaymentActionStatusSuccess PaymentActionStatus = "success"
-	PaymentActionStatusFailed  PaymentActionStatus = "failed"
-)
-
-type Authorization struct {
-	RequestID     uuid.UUID
-	PaymentSource PaymentSource
-	Amount        Amount
-}
-
-type Capture struct {
-	RequestID       uuid.UUID
-	AuthorizationID uuid.UUID
-	Amount          Amount
-}
-
-type Refund struct {
-	RequestID       uuid.UUID
-	AuthorizationID uuid.UUID
-	Amount          Amount
-}
-
-type Void struct {
-	RequestID       uuid.UUID
-	AuthorizationID uuid.UUID
-}
-
 type Amount struct {
 	MinorUnits uint64
 	Currency   string
@@ -83,31 +40,6 @@ type Transaction struct {
 	CapturedAmount       Amount
 	RefundedAmount       Amount
 	PaymentActionSummary []*PaymentAction
-}
-
-// TODO: can pull this to different package
-type PaymentAction struct {
-	Type          PaymentActionType
-	Status        PaymentActionStatus
-	ProcessedDate time.Time
-	Amount        *Amount
-	RequestID     uuid.UUID
-}
-
-func (p PaymentAction) AuthorizationSuccess() bool {
-	return p.Type == PaymentActionTypeAuthorization && p.Status == PaymentActionStatusSuccess
-}
-
-func (p PaymentAction) VoidSuccess() bool {
-	return p.Type == PaymentActionTypeVoid && p.Status == PaymentActionStatusSuccess
-}
-
-func (p PaymentAction) CaptureSuccess() bool {
-	return p.Type == PaymentActionTypeCapture && p.Status == PaymentActionStatusSuccess
-}
-
-func (p PaymentAction) RefundSuccess() bool {
-	return p.Type == PaymentActionTypeRefund && p.Status == PaymentActionStatusSuccess
 }
 
 func (t Transaction) AuthorizationDate() (time.Time, error) {
